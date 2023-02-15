363310f029ae8670c81c3103c9e536161cad4e7b
Sagi Twig <twigsagi@gmail.com>
2023-02-15 18:28:49 +0200

    Add auth param from front

--- api/index.py
+++ api/index.py
@@ -31,13 +31,13 @@ def hello():
 def login():
 
     data = request.get_json()
-    username = data['username']
-    password = data['password']
-    auth = base64.b64encode(f'{username}:{password}'.encode('utf-8')).decode('utf-8')
+    auth = data['auth']
+    print(auth)
+    # auth = base64.b64encode(f'{username}:{password}'.encode('utf-8')).decode('utf-8')
     app.auth = auth
 
     headers = {
-        'Authorization': f'Basic {app.auth}',
+        'Authorization': f'Basic {auth}',
         'Content-Type': 'application/json',
         "Content-Encoding": "gzip",
         "Access-Control-Allow-Origin": "*",
@@ -55,104 +55,101 @@ def login():
 
 
 
-@app.route('/jira/project', methods=['POST', 'GET'])
-def get_all_projects():
+@app.route('/jira/project/<auth>', methods=['POST', 'GET'])
+def get_all_projects(auth):
     # Extract the page parameters from the query string
     page = request.args.get('page', default=1, type=int)
   
     # Base64 encode the credentials
     # auth = base64.b64encode(b'sagi.twig:St123369').decode('utf-8')
-    if app.auth:
 
-        # Set the Authorization header
-        headers = {
-            'Authorization': f'Basic {app.auth}',
-            'Content-Type': 'application/json',
-            "Content-Encoding": "gzip",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization"
-        }
+    # Set the Authorization header
+    headers = {
+        'Authorization': f'Basic {auth}',
+        'Content-Type': 'application/json',
+        "Content-Encoding": "gzip",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    }
 
-        # Send the API request with the startAt and maxResults parameters
-        response = requests.get(f'https://jira-soft.ngsoft.com/rest/api/2/project?startAt={(page - 1) * ITEMS_PER_PAGE}&maxResults={ITEMS_PER_PAGE}', headers=headers)
+    # Send the API request with the startAt and maxResults parameters
+    response = requests.get(f'https://jira-soft.ngsoft.com/rest/api/2/project?startAt={(page - 1) * ITEMS_PER_PAGE}&maxResults={ITEMS_PER_PAGE}', headers=headers)
 
-        string_data = response.content.decode("utf-8")
+    string_data = response.content.decode("utf-8")
 
-        # Return the response
-        return make_response(string_data, 200)
-    else:
-        return jsonify({"success": False})
+    # Return the response
+    return make_response(string_data, 200)
 
 
-@app.route('/jira/search/<project_id>/<severity>', methods=['POST', 'GET'])
-def jira_jql_severity(project_id, severity):
+
+@app.route('/jira/search/<project_id>/<severity>/<auth>', methods=['POST', 'GET'])
+def jira_jql_severity(project_id, severity, auth):
    
     # Base64 encode the credentials
     # auth = base64.b64encode(b'sagi.twig:St123369').decode('utf-8')
     page = request.args.get('page', default=1, type=int)
 
-    if app.auth:
-
-        # Set the Authorization header
-        headers = {
-            'Authorization': f'Basic {app.auth}',
-            'Content-Type': 'application/json',
-            "Content-Encoding": "gzip",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization"
-        }
 
 
-        # Send the API request
-        response = requests.get(f'{BASE_URL}/search/?jql=project={project_id}&startAt={(page - 1) * ITEMS_PER_PAGE}&maxResults={ITEMS_PER_PAGE}', headers=headers)
-        string_data = response.content.decode("utf-8")
-
-        total_issues = get_total_issues(f'project={project_id}', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_blocker = get_total_issues(f'project={project_id} AND severity="blocker"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_critical = get_total_issues(f'project={project_id} AND severity="critical"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_major = get_total_issues(f'project={project_id} AND severity="major"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_minor = get_total_issues(f'project={project_id} AND severity="minor"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_cosmetic = get_total_issues(f'project={project_id} AND severity="cosmetic"', headers, BASE_URL, NO_RESULT_SEARCH)
-
+    # Set the Authorization header
+    headers = {
+        'Authorization': f'Basic {auth}',
+        'Content-Type': 'application/json',
+        "Content-Encoding": "gzip",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    }
 
 
-        total_issues_open = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")} AND status="open"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_closed = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="closed"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_reopened = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="reopened"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_in_progress = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="in progress"', headers, BASE_URL, NO_RESULT_SEARCH)
-        total_issues_customer_approval = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="customer approval"', headers, BASE_URL, NO_RESULT_SEARCH)
+    # Send the API request
+    response = requests.get(f'{BASE_URL}/search/?jql=project={project_id}&startAt={(page - 1) * ITEMS_PER_PAGE}&maxResults={ITEMS_PER_PAGE}', headers=headers)
+    string_data = response.content.decode("utf-8")
+
+    total_issues = get_total_issues(f'project={project_id}', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_blocker = get_total_issues(f'project={project_id} AND severity="blocker"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_critical = get_total_issues(f'project={project_id} AND severity="critical"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_major = get_total_issues(f'project={project_id} AND severity="major"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_minor = get_total_issues(f'project={project_id} AND severity="minor"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_cosmetic = get_total_issues(f'project={project_id} AND severity="cosmetic"', headers, BASE_URL, NO_RESULT_SEARCH)
 
 
-        json_data = json.loads(string_data)
-        project_name = 'Error No Issues Found'
-        
-        if json_data['issues']:
-            project_name = json_data['issues'][0]['fields']['project']['name']
+
+    total_issues_open = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")} AND status="open"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_closed = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="closed"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_reopened = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="reopened"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_in_progress = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="in progress"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_customer_approval = get_total_issues(f'project={project_id} {(f"AND severity={severity}" if severity != "All" else "")}  AND status="customer approval"', headers, BASE_URL, NO_RESULT_SEARCH)
 
 
-        res = {
-            "data": string_data,
-            "total_issues" : total_issues,
-            "total_blocker" : total_issues_blocker,
-            "total_critical" : total_issues_critical,
-            "total_major" : total_issues_major,
-            "total_minor" : total_issues_minor,
-            "total_cosmetic" : total_issues_cosmetic,
-            "total_open" : total_issues_open,
-            "total_closed" : total_issues_closed,
-            "total_reopened" : total_issues_reopened,
-            "total_in_progress" : total_issues_in_progress,
-            "total_customer_approval" : total_issues_customer_approval,
-            "project_name" : project_name,
-        }
+    json_data = json.loads(string_data)
+    project_name = 'Error No Issues Found'
+    
+    if json_data['issues']:
+        project_name = json_data['issues'][0]['fields']['project']['name']
 
 
-        # Return the response
-        return make_response(res, 200)
-    else:
-        return jsonify({"success": False})
+    res = {
+        "data": string_data,
+        "total_issues" : total_issues,
+        "total_blocker" : total_issues_blocker,
+        "total_critical" : total_issues_critical,
+        "total_major" : total_issues_major,
+        "total_minor" : total_issues_minor,
+        "total_cosmetic" : total_issues_cosmetic,
+        "total_open" : total_issues_open,
+        "total_closed" : total_issues_closed,
+        "total_reopened" : total_issues_reopened,
+        "total_in_progress" : total_issues_in_progress,
+        "total_customer_approval" : total_issues_customer_approval,
+        "project_name" : project_name,
+    }
+
+
+    # Return the response
+    return make_response(res, 200)
+
 
 
 
